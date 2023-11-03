14d0596d949c9a40b7a6c6d8da59f10daab4cdea
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2023-11-03 23:07:11 +0000

    can add configs to users fixed wrong things can acc connect

--- nea/server.py
+++ nea/server.py
@@ -1,62 +1,82 @@
 import socket
 import sqlite3
-from datetime import datetime 
+from datetime import datetime
+import pickle
 
 con = sqlite3.connect("users.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE Accounts(User_ID, Usernames, Passwords, Date_Created,settings,highest_score)")
+#cur.execute("CREATE TABLE Accounts(Username, Password, Date_Created,settings,highest_score)")
 
-number_of_accounts = cur.execute("SELECT COUNT(*) FROM Accounts")
+
 
 def check_user_pass(received_data):
-  res = cur.execute(f"SELECT Usernames FROM Accounts WHERE {received_data}")# check if pass and user of same entity matches recirved data
-  return res
+    cur.execute("SELECT Username FROM Accounts WHERE Username=? AND Password=?",(received_data[1],received_data[2]))  # check if pass and user of same entity matches recirved data
+    if cur.fetchone() == None:
+        return False
+    else:
+        return True
+
+def check_user(received_data):
+    cur.execute("SELECT Username FROM Accounts WHERE Username=?",received_data[1])
+    if cur.fetchone() == None:
+        return False
+    else:
+        return True
 
 def add_data(received_data):
-  if cur.execute(f"SELECT Usernames FROM Accounts WHERE {received_data[0]}") :
-    return True
-  else:
-    cur.execute(f"INSERT INTO Accounts ('{number_of_accounts+1}','{received_data[0]}','{received_data[1]}','{datetime.now()}')")#received_data[2] is login or signup received_data[3] 456 is rest
+        cur.execute("INSERT INTO Accounts (Username, Password, Date_Created,settings,highest_score) VALUES (?,?,?,?,?)",(received_data[1],received_data[2],datetime.now(),received_data[3],received_data[4]))  # received_data[2] is login or signup received_data[3] 456 is rest
+        con.commit()
+
+
+def update_config(received_data):
+    cur.execute("UPDATE users SET settings = ? WHERE Username = ?", (received_data[2], received_data[1]))
     con.commit()
-    return 1
-    
 
-s = socket.socket()         
-print ("Socket successfully created")
 
-port = 12345# can be anything
 
-s.bind(('0.0.0.0', port))         
-print ("socket binded to %s" %(port)) 
+s = socket.socket()
+print("Socket successfully created")
 
-s.listen(5)     
-print ("socket is listening") # max queue of 5 connections any more is rejected limited by hardware
+port = 2000  # can be anything
 
-while True: 
- 
-# Establish connection with client. 
-  c, addr = s.accept()     
-  print ('Got connection from', addr )
- 
-  # send a thank you message to the client. encoding to send byte type. 
-  #c.send('Thank you for connecting'.encode()) 
-  
-  data = client_socket.recv(1024)  # Receive up to 1024 bytes of data
-  received_data = data.decode('utf-8')
+s.bind(("0.0.0.0", port))
+print(f"socket binded to {port}")
 
-  if received_data[2]=="login":
-    if check_user_pass(received_data):
-      c.send('True'.encode())
-    else:
-      c.send('False'.encode())
-  if received_data[2]=="signup":
-    x=  add_data(received_data)
-    if x:
-      c.send('False'.encode())
-    if x==1:
-      c.send('True'.encode())
- 
-  # Close the connection with the client 
-  c.close()
-   
-  
+s.listen(5)
+print("socket is listening")  # max queue of 5 connections any more is rejected limited by hardware
+
+while True:
+
+    # Establish connection with client.
+    sock, address = s.accept()
+    print("Got connection from", address)
+
+
+
+    data = sock.recv(1024)  # Receive up to 1024 bytes of data
+    received_data = pickle.loads(data)
+
+    if received_data[0] == "login":
+        if check_user_pass(received_data):
+            sock.send("True".encode())
+            sock.close()
+        else:
+            sock.send("False".encode())
+            sock.close()
+
+    if received_data[0] == "signup":
+        if check_user(received_data):
+            sock.send("False".encode())
+            sock.close()
+
+        else:
+            add_data(received_data)
+            sock.send("True".encode())
+            sock.close()
+    if received_data[0]=="config":
+        update_config(received_data)
+        sock.close()
+
+
+
+
