e2e94d6f6da326c3bbe39d1299073a2aa58c1cf4
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2023-11-03 11:15:29 +0000

    Added accounts table

--- nea/server.py
+++ nea/server.py
@@ -3,4 +3,4 @@ import sqlite3
 
 con = sqlite3.connect("users.db")
 cur = con.cursor()
-cur.execute("CREATE TABLE movie(title, year, score)")
+cur.execute("CREATE TABLE Accounts(User_ID, Usernames, Passwords, Date_Created,settings)")
