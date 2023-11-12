5ffaadc2f7f331bf9b26fb42327a70b37afa3e3d
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2023-11-12 17:44:01 +0000

    ?

--- nea/server.py
+++ nea/server.py
@@ -13,6 +13,7 @@ except:
 
 def check_user_pass(received_data):
     cur.execute("SELECT Username FROM Accounts WHERE Username=? AND Password=?",(received_data[1],received_data[2]))  # check if pass and user of same entity matches recirved data
+
     if cur.fetchone() == None:
         return False
     else:
@@ -35,7 +36,12 @@ def update_config(received_data):
 def fetch_hours(received_data):
     cur.execute("SELECT hours_played FROM Accounts WHERE Username=?", (received_data[1],))
     hours = cur.fetchone()
-    return hours
+    return hours[0]
+
+def fetch_settiings(received_data):
+    cur.execute("SELECT settings FROM Accounts WHERE Username=?", (received_data[1],))
+    settings = cur.fetchone()
+    return settings[0]
 def update_hours(received_data):
     cur.execute("SELECT hours_played,minutes FROM Accounts WHERE Username=?",(received_data[1],))
     hours,minutes = cur.fetchone()
@@ -52,7 +58,7 @@ def update_hours(received_data):
 s = socket.socket()
 print("Socket successfully created")
 
-port = 3000  # can be anything
+port = 3100  # can be anything
 s.bind(("0.0.0.0", port))
 print(f"socket binded to {port}")
 
@@ -73,7 +79,9 @@ while True:
     if received_data[0] == "login":
         print("login")
         if check_user_pass(received_data):
-            sock.send("True".encode())
+            print(fetch_settiings(received_data))
+            data = pickle.dumps(["True", fetch_settiings(received_data)])
+            sock.send(data)
             sock.close()
         else:
             sock.send("False".encode())
@@ -102,5 +110,5 @@ while True:
         sock.close()
     if received_data[0]=="fetch":
         print("fetch")
-        sock.send(str(fetch_hours(received_data)[0]).encode())
+        sock.send(str(fetch_hours(received_data)).encode())
         sock.close()
