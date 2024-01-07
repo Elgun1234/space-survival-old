d6d39e2b390fc2f52585388c3e43846460a9f385
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2024-01-07 22:33:44 +0000

    fixed hours and more columns and split table

--- nea/server.py
+++ nea/server.py
@@ -1,18 +1,23 @@
 import socket
 import sqlite3
-
+from datetime import datetime, timedelta
 import pickle
+import hashlib
 
 con = sqlite3.connect("users.db")
 cur = con.cursor()
 try:
-    cur.execute("CREATE TABLE Accounts(Username, Password, hours_played,minutes,settings,highest_score)")
+    cur.execute("CREATE TABLE Accounts(Username, Password)")
+except:
+    pass
+try:
+    cur.execute("CREATE TABLE Information(Username,playtime,settings,highest_score,total_score,runs,bullets_shot)")
 except:
     pass
 
 
 def check_user_pass(received_data):
-    cur.execute("SELECT Username FROM Accounts WHERE Username=? AND Password=?",(received_data[1],received_data[2]))  # check if pass and user of same entity matches recirved data
+    cur.execute("SELECT Username FROM Accounts WHERE Username=? AND Password=?",(received_data[1],hashlib.sha512(received_data[2].encode(encoding = 'UTF-8', errors = 'strict')).digest())) # check if pass and user of same entity matches recirved data
 
     if cur.fetchone() == None:
         return False
@@ -27,30 +32,45 @@ def check_user(received_data):
         return True
 
 def add_data(received_data):
-        cur.execute("INSERT INTO Accounts (Username, Password, hours_played,minutes,settings,highest_score) VALUES (?,?,?,?,?,?)",(received_data[1],received_data[2],0,0,received_data[3],received_data[4]))  # received_data[2] is login or signup received_data[3] 456 is rest
+        cur.execute("INSERT INTO Accounts (Username, Password) VALUES (?,?)",(received_data[1],hashlib.sha512(received_data[2].encode(encoding = 'UTF-8', errors = 'strict')).digest()))  # look at signup in main
+        cur.execute("INSERT INTO Information (Username,playtime, settings, highest_score,total_score,runs,bullets_shot) VALUES (?,?,?,?,?,?,?)",(received_data[1],str(timedelta()),received_data[3],0,0,0,0))#0
         con.commit()
 def update_config(received_data):
-    cur.execute("UPDATE Accounts SET settings = ? WHERE Username = ?", (received_data[2], received_data[1]))
+    cur.execute("UPDATE Information SET settings = ? WHERE Username = ?", (received_data[2], received_data[1]))
     con.commit()
 
 def fetch_hours(received_data):
-    cur.execute("SELECT hours_played FROM Accounts WHERE Username=?", (received_data[1],))
-    hours = cur.fetchone()
-    return hours[0]
+    cur.execute("SELECT playtime,total_score,runs,bullets_shot ,highest_score FROM Information WHERE Username=?", (received_data[1],))
+
+
+
+    return cur.fetchone()
 
 def fetch_settiings(received_data):
-    cur.execute("SELECT settings FROM Accounts WHERE Username=?", (received_data[1],))
+    cur.execute("SELECT settings FROM Information WHERE Username=?", (received_data[1],))
     settings = cur.fetchone()
     return settings[0]
-def update_hours(received_data):
-    cur.execute("SELECT hours_played,minutes FROM Accounts WHERE Username=?",(received_data[1],))
-    hours,minutes = cur.fetchone()
-    minutes+=1
-    if minutes // 60 == 1:
-        hours +=minutes // 60
-        minutes = 0
 
-    cur.execute("UPDATE Accounts SET hours_played = ?,minutes=? WHERE Username = ?", (hours,minutes, received_data[1]))
+def string_2_timedelta(string):
+    x = string.split(":")
+    return timedelta(hours=int(x[0]),minutes=int(x[1]),seconds=int(x[2]))
+
+def update_info(received_data):## datetimme
+    cur.execute("SELECT playtime,total_score,runs,bullets_shot ,highest_score FROM Information WHERE Username=?",(received_data[1],))
+    info = cur.fetchone()
+
+    new_time = string_2_timedelta(info[0]) + string_2_timedelta(received_data[2])
+
+    new_total = info[1]+received_data[3]
+    print(info[2])
+    runs =1+info[2]
+    new_bullet = info[3]+received_data[4]
+    if info[4]<received_data[3]:
+        cur.execute("UPDATE Information SET playtime = ?,total_score= ?,runs= ?,bullets_shot= ?,highest_score= ? WHERE Username = ?", (str(new_time), new_total, runs, new_bullet,received_data[3], received_data[1]))
+    else:
+
+        cur.execute("UPDATE Information SET playtime = ?,total_score= ?,runs= ?,bullets_shot= ? WHERE Username = ?", (str(new_time),new_total,info[2],new_bullet, received_data[1]))
+
     con.commit()
 
 
@@ -79,7 +99,7 @@ while True:
     if received_data[0] == "login":
         print("login")
         if check_user_pass(received_data):
-            print(fetch_settiings(received_data))
+
             data = pickle.dumps(["True", fetch_settiings(received_data)])
             sock.send(data)
             sock.close()
@@ -106,7 +126,7 @@ while True:
         sock.close()
     if received_data[0]=="hours":
         print("hours")
-        update_hours(received_data)
+        update_info(received_data)
         sock.close()
     if received_data[0]=="fetch":
         print("fetch")
